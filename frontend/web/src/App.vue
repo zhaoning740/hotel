c29599ff06dbf16d5bdff9aebe9f64b8635b13db
baylin87 <frontend@binlv.top>
2019-04-30 20:19:23 +0800

    feat: project init

---
+++ frontend/web/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="App">
+    Hello World
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import './assets/styles/var.scss';
+.App {
+  color: $primary-color;
+}
+</style>
---
+++ frontend/web/webpack/webpack.config.js
@@ -0,0 +1,144 @@
+const path = require('path');
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const CleanPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    main: ["@babel/polyfill", path.resolve(__dirname, '../src/main.js')]
+  },
+  output: {
+    path: path.resolve(process.cwd(), 'dist'),
+    filename: 'js/[name].[hash:8].js',
+  },
+  devServer: {
+    // ...
+  },
+  resolve: {
+    alias: {
+      // vue$: 'vue/dist/vue.runtime.esm.js'
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        use: [
+          {
+            loader: 'cache-loader'
+          },
+          {
+            loader: 'thread-loader'
+          },
+          {
+            loader: 'vue-loader',
+            options: {
+              compilerOptions: {
+                preserveWhitespace: false
+              },
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(js|jsx)$/,
+        use: [
+          {
+            loader: 'cache-loader'
+          },
+          {
+            loader: 'thread-loader'
+          },
+          {
+            loader: 'babel-loader'
+          }
+        ]
+      },
+      {
+        test: /\.(scss|sass)$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              implementation: require('dart-sass')
+            }
+          },
+          {
+            loader: 'postcss-loader'
+          }
+        ],
+      },
+      {
+        test: /\.(jpe?g|png|gif)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 4096,
+              fallback: {
+                loader: 'file-loader',
+                options: {
+                    name: 'img/[name].[hash:8].[ext]'
+                }
+              }
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 4096,
+              fallback: {
+                loader: 'file-loader',
+                options: {
+                  name: 'media/[name].[hash:8].[ext]'
+                }
+              }
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 4096,
+              fallback: {
+                loader: 'file-loader',
+                options: {
+                  name: 'fonts/[name].[hash:8].[ext]'
+                }
+              }
+            }
+          }
+        ]
+      },
+    ],
+  },
+  plugins: [
+    new CleanPlugin({
+      // 不需要清空
+      cleanOnceBeforeBuildPatterns: ['**/*']
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, '../public/index.html')
+    }),
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new VueLoaderPlugin()
+  ],
+}
