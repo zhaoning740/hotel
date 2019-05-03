7223b69bd2cd30e6274c3b7d8eb7e66a79775461
zhaoning740 <zhaoning740@163.com>
2019-05-03 12:46:31 +0800

    feat: USER LIST

--- frontend/web/src/view/User/index.vue
+++ frontend/web/src/view/User/index.vue
@@ -1,22 +1,109 @@
 <template>
   <div>
-    <span>helloUser</span>
+    <div class="buttonContainer">
+      <Button type="primary" to="/shop/create">新增用户</Button>
+    </div>
+    <Table border :columns="columns" :data="data"></Table>
   </div>
 </template>
 
 <script>
+import R from '@/request';
+
 export default {
-  name: 'Test',
+  name: "Test",
 
   data() {
     return {
+      columns: [
+        {
+          title: '用户名',
+          key: "username",
+        },
+        {
+          title: "联系电话",
+          key: "phone"
+        },
+        {
+          title: "地址",
+          key: "address"
+        },
+        {
+          title: "操作",
+          key: "action",
+          width: 150,
+          align: "center",
+          render: (h, params) => {
+            return h("div", [
+              h(
+                "Button",
+                {
+                  props: {
+                    type: "primary",
+                    size: "small"
+                  },
+                  style: {
+                    marginRight: "5px"
+                  },
+                  on: {
+                    click: () => {
+                      this.show(params.id);
+                    }
+                  }
+                },
+                "编辑"
+              ),
+              h(
+                "Button",
+                {
+                  props: {
+                    type: "error",
+                    size: "small"
+                  },
+                  on: {
+                    click: () => {
+                      this.remove(params.index);
+                    }
+                  }
+                },
+                "删除"
+              )
+            ]);
+          }
+        }
+      ],
+      data: []
     };
   },
-  computed: {
+  computed: {},
+  methods: {
+    fetchList() {
+      R.post('/user/userList')
+      .then(res => {
+        if(res.data && Array.isArray(data)) {
+          this.data = res.data;
+        }
+      })
+    },
+    show(index) {
+      this.$Modal.info({
+        title: "用户详情",
+        content: `test`
+      });
+    },
+    remove(index) {
+      this.data.splice(index, 1);
+    },
+  },
+  mounted() {
+    this.fetchList()
   }
 };
 </script>
 
 <style scoped>
-
+.buttonContainer{
+  text-align: right;
+  margin-bottom: 10px;
+}
 </style>
